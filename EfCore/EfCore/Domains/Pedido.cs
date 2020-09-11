fbc78f7627610c7c3a4628f275eef2a1ecd7a8fd
henrique-ol-dev <riqueneguin467@gmail.com>
2020-09-11 15:03:06 -0300

    Code first aula06

---
+++ EfCore/EfCore/Domains/Pedido.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EfCore.Domains
+{
+  
+    public class Pedido
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string Status { get; set; }
+        public DateTime OrderDate { get; set; }
+
+        public Pedido()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
---
+++ EfCore/EfCore/Domains/Produto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EfCore.Domains
+{
+    /// < summmary >
+    /// Definir uma classe produto
+    /// </ summary >
+    public class Produto
+    {
+    [Key] 
+    public Guid Id { get; set; }
+    public string Nome { get; set;}
+    public float Preco { get; set; }
+
+    public Produto ()
+    {
+        Id = Guid.NewGuid();
+    }
+}
+}
